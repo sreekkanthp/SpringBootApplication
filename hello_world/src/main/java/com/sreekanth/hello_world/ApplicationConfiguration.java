55dab36417b69cfa5257f11023fe448a1188f148
sreekkantp <sreekkantp@gmail.com>
2019-10-06 14:55:01 +0530

    First check-in of the spring boot app

---
+++ hello_world/src/main/java/com/sreekanth/hello_world/ApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package com.sreekanth.hello_world;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ApplicationConfiguration {
+@RequestMapping("hello")
+public String hello()
+{
+	return "Hello World !!!";
+}
+
+}
