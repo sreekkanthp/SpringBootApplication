a9ef49f3e248fdfd3d304a50b6e233df8e2f4ad5
sreekkantp <sreekkantp@gmail.com>
2019-10-12 19:40:56 +0530

    Change on 10/12

--- hello_world/src/main/java/com/sreekanth/hello_world/ApplicationConfiguration.java
+++ hello_world/src/main/java/com/sreekanth/hello_world/ApplicationConfiguration.java
@@ -6,7 +6,7 @@ public class ApplicationConfiguration {
 @RequestMapping("hello")
 public String hello()
 {
-	return "Welcome to the world of Devops !!!";
+	return "This is my First Build !!!";
 }
 
 }
